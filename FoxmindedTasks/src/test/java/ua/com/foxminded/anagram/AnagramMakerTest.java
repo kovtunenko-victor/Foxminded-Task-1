dc8dbd7bf873b77b0d5bf9588d2862e8bec459f6
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2020-04-16 14:20:17 +0300

    migrate to JUnit 5

--- FoxmindedTasks/src/test/java/ua/com/foxminded/anagram/AnagramMakerTest.java
+++ FoxmindedTasks/src/test/java/ua/com/foxminded/anagram/AnagramMakerTest.java
@@ -1,23 +1,18 @@
 package ua.com.foxminded.anagram;
 
-import static org.junit.Assert.assertEquals;
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 public class AnagramMakerTest {
-   @Rule
-   public ExpectedException thrown = ExpectedException.none();
-
    private final AnagramMaker anagramMaker = new AnagramMaker();
 
    @Test
-   public void makeAnagramShouldReturnSourceStringIfSentenceContainsOnlyNumbers() { 
+   public void makeAnagramShouldReturnSourceStringIfSentenceContainsOnlyNumbers() {
       String expected = "1234 5678";
       String actual = anagramMaker.makeAnagram("1234 5678");
-      
+
       assertEquals(expected, actual);
    }
 
@@ -25,15 +20,15 @@ public class AnagramMakerTest {
    public void makeAnagramShouldReturnRevertedSymbolsIfSentenceContainsOnlyLetterSymbols() {
       String expected = "dcba hgfe";
       String actual = anagramMaker.makeAnagram("abcd efgh");
-      
+
       assertEquals(expected, actual);
    }
-   
+
    @Test
    public void makeAnagramShouldReturnRevertedLetterSymbolsAndNonLetterStayedInPlaceIfSentenceContainsMixedLetterAndNonLetterSymbols() {
       String expected = "d1cba hgf!e";
       String actual = anagramMaker.makeAnagram("a1bcd efg!h");
-      
+
       assertEquals(expected, actual);
    }
 
@@ -41,55 +36,59 @@ public class AnagramMakerTest {
    public void makeAnagramShouldReturnSourceSymbolIfSourceStringIsOneSymbol() {
       String expected = "a";
       String actual = anagramMaker.makeAnagram("a");
-      
+
       assertEquals(expected, actual);
    }
-   
+
    @Test
    public void makeAnagramShouldReturnSourceStringIfWordContainsSameSymbols() {
       String expected = "aaaa";
       String actual = anagramMaker.makeAnagram("aaaa");
-      
+
       assertEquals(expected, actual);
    }
-   
+
    @Test
    public void makeAnagramShouldReturnRevertedSymbolsIfWordContainsLetterSymbols() {
       String expected = "abcd";
       String actual = anagramMaker.makeAnagram("dcba");
-      
+
       assertEquals(expected, actual);
    }
-   
+
    @Test
    public void makeAnagramShouldReturnNotRevertedSymbolsIfWordContainsNonLetterSymbols() {
       String expected = "!@#$%";
       String actual = anagramMaker.makeAnagram("!@#$%");
-      
+
       assertEquals(expected, actual);
    }
 
    @Test
    public void makeAnagramShouldThrowExceptionIfSentenceIsEmptyString() {
-      thrown.expect(IllegalArgumentException.class);
-      thrown.expectMessage(is("Sentence is empty or contains only tabulation symbols"));
-      
-      anagramMaker.makeAnagram("");
+      Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+         anagramMaker.makeAnagram("");
+      });
+
+      assertEquals("Sentence is empty or contains only tabulation symbols", exception.getMessage());
    }
    
    @Test
    public void makeAnagramShouldThrowExceptionIfSentenceConsistsOfTabulation() {
-      thrown.expect(IllegalArgumentException.class);
-      thrown.expectMessage(is("Sentence is empty or contains only tabulation symbols"));
-      
-      anagramMaker.makeAnagram("    ");
-   }
+      Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+         anagramMaker.makeAnagram("    ");
+      });
 
+      assertEquals("Sentence is empty or contains only tabulation symbols", exception.getMessage());
+   }
+   
    @Test
    public void makeAnagramShouldThrowExceptionIfSentenceIsNull() {
-      thrown.expect(IllegalArgumentException.class);
-      thrown.expectMessage(is("Sentence is null"));
-      
-      anagramMaker.makeAnagram(null);
+      Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+         anagramMaker.makeAnagram(null);
+      });
+
+      assertEquals("Sentence is null", exception.getMessage());
    }
+
 }
